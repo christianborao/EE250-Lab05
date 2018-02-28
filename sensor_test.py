6b5dfeef5c797af7a15ac105b221d78db8704069
student <borao@usc.edu>
2018-02-27 18:07:14 -0800

    Added Adafruit stuff and modified sensor_test.py

--- sensor_test.py
+++ sensor_test.py
@@ -1,7 +1,14 @@
 
+import time
 
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
 
-
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 led_pin = 2 #pin LED is connected to
 light_pin = 0 #pin light sensor is connected to
