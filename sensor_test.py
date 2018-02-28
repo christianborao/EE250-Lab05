1c9bf41b65617444756fdeeb079a9c98c9a045a4
Christian Borao <35749566+christianborao@users.noreply.github.com>
2018-02-27 22:46:23 -0800

    Update sensor_test.py

--- sensor_test.py
+++ sensor_test.py
@@ -1,11 +1,21 @@
+import time
 
-#import something, I'm sure
+import RPi.GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
 
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
+GPIO.setmode(GPIO.BCM)
 
-led_pin = 2 #pin LED is connected to
+led_pin = 17 #pin LED is connected to
 light_pin = 0 #pin light sensor is connected to
 sound_pin = 1 #pin that sound sensor is connected to
+
+GPIO.setup(led_pin, GPIO.OUT)
 
 def Main():
 
@@ -30,20 +40,20 @@ def Main():
 			print("Light level: " + str(light_data))
 
 			#if output is less than 400 threshold, print "dark"
-			if (light < 400)
+			if (light_data < 400):
 				print("dark")
 		
 			#else, print "bright"
-			else
+			else:
 				print("bright")
 
 			#delay 100ms
 			time.sleep(0.1)
 
 		for x in range(8):
-			if (x % 2 == 0)
+			if (x % 2 == 0):
 				GPIO.output(led_pin, GPIO.HIGH) #turn LED on
-			else
+			else:
 				GPIO.output(led_pin, GPIO.LOW) #turn LED off
 
 			#delay 100ms
@@ -57,20 +67,19 @@ def Main():
 			#print output:
 			print("Sound level: " + str(sound_data))
 
-			if (sound_data > 300)
+			if (sound_data > 300):
 				GPIO.output(led_pin, GPIO.HIGH) #LED on
 				time.sleep(0.1) #delay for 100ms
 				GPIO.output(led_pin, GPIO.LOW) #LED off
-			else
+			else:
 				time.sleep(0.1) #delay for 100ms
 
 		for x in range(8):
-			if (x % 2 == 0)
+			if (x % 2 == 0):
 				GPIO.output(led_pin, GPIO.HIGH) #LED on
-			else
+			else:
 				GPIO.output(led_pin, GPIO.LOW) #LED off
 			time.sleep(0.1) #delay 100ms
 
 if __name__ == '__main__':
 		Main()
-
